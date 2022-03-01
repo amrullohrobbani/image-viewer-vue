156ccb131869ed0e9f8cdf48f3a42bfc80709e02
Amrulloh Robbani <amrullohrobbani@gmail.com>
2022-03-01 16:53:47 +0700

    thumbnail edit

--- src/components/ImageViewer/Modal/LightboxModal.vue
+++ src/components/ImageViewer/Modal/LightboxModal.vue
@@ -4,30 +4,33 @@
             <div class="close" @click.stop="lightbox.hide">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
             </div>
-            <div class="flex">
-                <div class="lightbox-image" @click.stop="">
-                    <img :src="images[lightbox.index].url">
+            <div class="flex-column">
+                <div class="flex-column-grow">
+                    <div class="lightbox-image" @click.stop="">
+                        <img :src="images[lightbox.index].url">
+                    </div>
+                    <div class="urlText">{{images[lightbox.index].url}}</div>
                 </div>
+                <div class="lightbox-thumbnail">
+                    <div class="prev"
+                        @click.stop="lightbox.prev">
+                        <svg class="pointer-events-none" fill="#fff" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
+                            <path d="M0-.5h24v24H0z" fill="none"/>
+                        </svg>
+                    </div>
+                    <div class="photos">
+                        <img v-for="(image, index) in images" :key="image.id" :src="image.url" :alt="alt" loading="lazy" @click.stop="lightbox.show(index)">
+                    </div>
+                    <div class="next"
+                        @click.stop="lightbox.next">
+                        <svg class="pointer-events-none" fill="#fff" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
+                            <path d="M0-.25h24v24H0z" fill="none"/>
+                        </svg>
+                    </div>
+                </div><!-- thumbnail -->
             </div>
-            <div class="lightbox-thumbnail">
-                <div class="prev"
-                    @click.stop="lightbox.prev">
-                    <svg class="pointer-events-none" fill="#fff" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
-                        <path d="M0-.5h24v24H0z" fill="none"/>
-                    </svg>
-                </div>
-                <div class="photos">
-                     <img v-for="(image, index) in images" :key="image.id" :src="image.url" :alt="alt" loading="lazy" @click.stop="lightbox.show(index)">
-                </div>
-                <div class="next"
-                    @click.stop="lightbox.next">
-                    <svg class="pointer-events-none" fill="#fff" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
-                        <path d="M0-.25h24v24H0z" fill="none"/>
-                    </svg>
-                </div>
-            </div><!-- thumbnail -->
         </div>
 </template>
 
@@ -72,15 +75,16 @@ export default {
     border-radius: 0px 0px 0px 40px;
 }
 .lightbox-image img {
+    padding: 30px;
     width: auto;
     height: auto;
     max-width: 100%;
-    max-height: calc(100vh - 25vh);
-    transition: all .2s ease-in-out;
+    max-height: calc(100vh - 35vh);
+    transition: opacity 0.3s;
+    -webkit-transition: opacity 0.3s;
 }
 .lightbox-thumbnail {
     background: rgba(0, 0, 0, 0.2);
-    position: fixed;
     left: 0;
     bottom: 0;
     z-index: 50;
@@ -105,10 +109,20 @@ export default {
     align-self: center;
     height: auto;
 }
-.flex {
+.flex-column {
     display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+}
+.flex-column-grow {
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 0;
 }
 .photos {
+    display:flex;
     justify-content: center;
     flex: 1 1 auto;
 }
@@ -142,4 +156,7 @@ export default {
   stroke-linecap: round;
   stroke-width: 5;
 }
+.urlText {
+    color: whitesmoke;
+ }
 </style>
